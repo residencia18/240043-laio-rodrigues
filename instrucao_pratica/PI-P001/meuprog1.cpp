861e067cca9f6a14ff32777670785b2a867dc0ed
OutLaio <rlaio.ege@gmail.com>
2023-08-24 01:58:19 -0300

    meuprog1.cpp

---
+++ instrucao_pratica/PI-P001/meuprog1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+    string nome;
+
+    cout << endl;
+    cout << "Digite seu nome: ";
+    cin >> nome;
+    cout << endl;
+    cout << "Bom dia " << nome << "!";
+
+    return 0;
+}
