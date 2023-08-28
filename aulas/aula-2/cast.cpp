19dfd7fbfa2590fcb5e9e3588226205a2a043407
OutLaio <rlaio.ege@gmail.com>
2023-08-28 16:26:23 -0300

    Aula 2

---
+++ aulas/aula-2/cast.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void){
+    float pi;
+    
+    pi = 3 +0.15152;
+
+    int intpi = pi;
+
+    cout << intpi;
+    cout << endl;
+
+    return 0;
+}
