6dc83dbcdc51f01cc47c5167ef89c6a524854834
Laio Rodrigues <rlaio.ege@gmail.com>
2023-11-23 17:37:38 -0300

    Fim do dia 23/11/2023

---
+++ modulo2/dotNet/Semana3/Aula/Tuplas/Program.cs
@@ -0,0 +1,4 @@
+﻿/*  Crie uma função que recebe um nome e uma idade como parâmetros e retorna 
+    uma tupla contendo o nome e a idade. Em seguida, chame a função com 
+    diferentes valores e exiba os resultados. */
+
