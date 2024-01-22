d309bd4817dd84f68cb2a5e3b8344a8db3cd41d8
OutLaio <rlaio.ege@gmail.com>
2024-01-22 09:41:23 -0300

    Trilha java, dia 22/01/2024

--- trilha_java/Programs/src/configuracao/Configuracao.java
+++ trilha_java/Programs/src/configuracao/Configuracao.java
@@ -1,7 +1,9 @@
 package configuracao;
 
 public class Configuracao {
-	String alfabeto;
+	private String alfabeto;
+	private int tamanhoSenha;
+	private int maxTentativas;
 
 	public String getAlfabeto() {
 		return alfabeto;
@@ -10,8 +12,41 @@ public class Configuracao {
 	public void setAlfabeto(String alfabeto) throws Exception {
 		if(alfabeto == null || alfabeto.length()<=1)
 			throw new Exception("O alfabeto deve possuir mais de 1 caracter");
+		testRepeat(alfabeto);
+		
 		this.alfabeto = alfabeto;
 	}
+	
+	private void testRepeat(String alfabeto) throws Exception {
+		for(int i = 0; i < alfabeto.length(); i++)
+			for(int j = i+1; j < alfabeto.length(); j++)
+				if(alfabeto.charAt(i) == alfabeto.charAt(j))
+					throw new Exception("Não podem haver caracteres repetidos no alfabeto");
+	}
+
+	public int getTamanhoSenha() {
+		return tamanhoSenha;
+	}
+
+	public void setTamanhoSenha(int tamanhoSenha) throws Exception {
+		if(tamanhoSenha < 0) throw new Exception("O tamanho da senha não pode ser negativo");
+		if(tamanhoSenha < 3) throw new Exception("A senha deve ter pelo menos 3 caracteres");
+		if(tamanhoSenha >= alfabeto.length()) throw new Exception("O tamanho da senha deve ser menor que o tamanho do alfabeto");
+		
+		this.tamanhoSenha = tamanhoSenha;
+	}
+
+	public int getMaxTentativas() {
+		return maxTentativas;
+	}
+
+	public void setMaxTentativas(int maxTentativas) throws Exception {
+		if(maxTentativas < 0) throw new Exception("O máximo de tentativas não pode ser negativo");
+		if(maxTentativas == 0) throw new Exception("O usuário deve ter ao menos uma tentativa");
+		
+		this.maxTentativas = maxTentativas;
+	}
+	
 	
 	
 }
--- trilha_java/Programs/src/configuracao/ConfiguracaoTest.java
+++ trilha_java/Programs/src/configuracao/ConfiguracaoTest.java
@@ -9,38 +9,128 @@ class ConfiguracaoTest {
 	@Test
 	void test() {
 		Configuracao configuracao = new Configuracao();
-		String senha = "ABCDEFGHIJ";
+		testSetAlfabeto(configuracao);
+		testSetTamSenha(configuracao);
+		testSetMaxTentativas(configuracao);
+		
+		
+		
+	}
+
+	private void testSetMaxTentativas(Configuracao configuracao) {
+		int maxTentativas;
+		maxTentativas = 4;
 		try {
-			configuracao.setAlfabeto(senha);
+			configuracao.setMaxTentativas(maxTentativas);
 		} catch (Exception e) {
 			fail("Gerou exceção indevida");
 			e.printStackTrace();
 		}
-		assertEquals(senha, configuracao.getAlfabeto());
+		assertEquals(maxTentativas, configuracao.getMaxTentativas());
 		
-		senha = null;
+		maxTentativas = -1;
 		try {
-			configuracao.setAlfabeto(senha);
+			configuracao.setMaxTentativas(maxTentativas);
+		} catch (Exception e) {
+			assertEquals("O máximo de tentativas não pode ser negativo", e.getMessage());
+		}
+		assertNotEquals(maxTentativas, configuracao.getMaxTentativas());
+		
+		maxTentativas = 0;
+		try {
+			configuracao.setMaxTentativas(maxTentativas);
+		} catch (Exception e) {
+			assertEquals("O usuário deve ter ao menos uma tentativa", e.getMessage());
+		}
+		assertNotEquals(maxTentativas, configuracao.getMaxTentativas());
+
+	}
+	
+	private void testSetTamSenha(Configuracao configuracao) {
+		int tamanhoSenha;
+		tamanhoSenha = 4;
+		try {
+			configuracao.setTamanhoSenha(tamanhoSenha);
+		} catch (Exception e) {
+			fail("Gerou exceção indevida");
+			e.printStackTrace();
+		}
+		assertEquals(tamanhoSenha, configuracao.getTamanhoSenha());
+		
+		tamanhoSenha = -1;
+		try {
+			configuracao.setTamanhoSenha(tamanhoSenha);
+		} catch (Exception e) {
+			assertEquals("O tamanho da senha não pode ser negativo", e.getMessage());
+		}
+		assertNotEquals(tamanhoSenha, configuracao.getTamanhoSenha());
+		
+		tamanhoSenha = 0;
+		try {
+			configuracao.setTamanhoSenha(tamanhoSenha);
+		} catch (Exception e) {
+			assertEquals("A senha deve ter pelo menos 3 caracteres", e.getMessage());
+		}
+		assertNotEquals(tamanhoSenha, configuracao.getTamanhoSenha());
+
+		tamanhoSenha = 2;
+		try {
+			configuracao.setTamanhoSenha(tamanhoSenha);
+		} catch (Exception e) {
+			assertEquals("A senha deve ter pelo menos 3 caracteres", e.getMessage());
+		}
+		assertNotEquals(tamanhoSenha, configuracao.getTamanhoSenha());
+		
+		tamanhoSenha = 11;
+		try {
+			configuracao.setTamanhoSenha(tamanhoSenha);
+		} catch (Exception e) {
+			assertEquals("O tamanho da senha deve ser menor que o tamanho do alfabeto", e.getMessage());
+		}
+		assertNotEquals(tamanhoSenha, configuracao.getTamanhoSenha());
+	}
+
+	private void testSetAlfabeto(Configuracao configuracao) {
+		String alfabeto = "ABCDEFGHIJ";
+		try {
+			configuracao.setAlfabeto(alfabeto);
+		} catch (Exception e) {
+			fail("Gerou exceção indevida");
+			e.printStackTrace();
+		}
+		assertEquals(alfabeto, configuracao.getAlfabeto());
+		
+		alfabeto = null;
+		try {
+			configuracao.setAlfabeto(alfabeto);
 		} catch (Exception e) {
 			assertEquals("O alfabeto deve possuir mais de 1 caracter", e.getMessage());
 		}
 		assertFalse(configuracao.getAlfabeto()==null);
 		
-		senha = "A";
+		alfabeto = "A";
 		try {
-			configuracao.setAlfabeto(senha);
+			configuracao.setAlfabeto(alfabeto);
 		} catch (Exception e) {
 			assertEquals("O alfabeto deve possuir mais de 1 caracter", e.getMessage());
 		}
 		assertFalse(configuracao.getAlfabeto().length()==1);
 		
-		senha = "";
+		alfabeto = "";
 		try {
-			configuracao.setAlfabeto(senha);
+			configuracao.setAlfabeto(alfabeto);
 		} catch (Exception e) {
 			assertEquals("O alfabeto deve possuir mais de 1 caracter", e.getMessage());
 		}
 		assertFalse(configuracao.getAlfabeto().length()==0);
+		
+		alfabeto = "ABCDEFGHIJA";
+		try {
+			configuracao.setAlfabeto(alfabeto);
+		} catch (Exception e) {
+			assertEquals("Não podem haver caracteres repetidos no alfabeto", e.getMessage());
+		}
+		assertNotEquals("ABCDEFGHIJA", configuracao.getAlfabeto());
 	}
 
 }
