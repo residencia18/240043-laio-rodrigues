a5d0e91474ed73428d169eb6c3997c50d1e54415
OutLaio <rlaio.ege@gmail.com>
2024-01-22 22:51:07 -0300

    PO-P07

---
+++ trilha_java/Praticas_Orientadas/PO-07/src/estudante/Estudante.java
@@ -0,0 +1,50 @@
+package estudante;
+
+public class Estudante {
+	private String nome;
+	private String cpf;
+	private float cra;
+	private int anoDeAdmissão;
+	
+	public Estudante(String nome, String cpf, float cra, int anoDeAdmissão) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.cra = cra;
+		this.anoDeAdmissão = anoDeAdmissão;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public float getCra() {
+		return cra;
+	}
+
+	public void setCra(float cra) {
+		this.cra = cra;
+	}
+
+	public int getAnoDeAdmissão() {
+		return anoDeAdmissão;
+	}
+
+	public void setAnoDeAdmissão(int anoDeAdmissão) {
+		this.anoDeAdmissão = anoDeAdmissão;
+	}
+	
+	
+}
---
+++ trilha_java/Praticas_Orientadas/PO-07/src/persistencia/EstudanteJSON.java
@@ -0,0 +1,18 @@
+package persistencia;
+
+import java.util.ArrayList;
+
+import estudante.Estudante;
+
+import org.json.*;
+
+public class EstudanteJSON {
+	JSONObject my_obj = new JSONObject();
+	
+	public void salvaEstudantes(ArrayList<Estudante> listaDeEstudantes, String nomeDoArquivo) {
+		
+		for (Estudante estudante : listaDeEstudantes) {
+			
+		}
+	}
+}
